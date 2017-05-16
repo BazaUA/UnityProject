31b7fdfb2852db8a39660fc15e5f4a739810a033
BazaUA <vbazalickyi@gmail.com>
2017-05-17 00:26:56 +0300

    practice #1

---
+++ Assets/Scripts/HeroRabit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HeroRabit : MonoBehaviour {
+	public float speed=1;
+	Rigidbody2D myBody;
+	SpriteRenderer mySprite;
+
+	// Use this for initialization
+	void Start () {
+		myBody = this.GetComponent<Rigidbody2D> ();
+		mySprite = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		float value = Input.GetAxis ("Horizontal");
+
+		if (Mathf.Abs (value) > 0) {
+			Vector2  vel = myBody.velocity;
+			vel.x = value * speed;
+			myBody.velocity = vel;
+		}
+		if (value < 0)
+			mySprite.flipX = true;
+		else if (value > 0)
+			mySprite.flipX = false;
+	}
+
+
+}
