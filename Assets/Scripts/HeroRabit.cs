e168054a6635fce0c4d555192370ad928855fdbe
BazaUA <vbazalickyi@gmail.com>
2017-05-22 23:54:07 +0300

    practice #2

--- Assets/Scripts/HeroRabit.cs
+++ Assets/Scripts/HeroRabit.cs
@@ -7,11 +7,19 @@ public class HeroRabit : MonoBehaviour {
 	public float speed=1;
 	Rigidbody2D myBody;
 	SpriteRenderer mySprite;
+	Animator animator;
+	bool isGrounded = false;
+	bool JumpActive = false;
+	float JumpTime = 0f;
+	public float MaxJumpTime = 2f;
+	public float JumpSpeed = 2f;
 
 	// Use this for initialization
 	void Start () {
 		myBody = this.GetComponent<Rigidbody2D> ();
 		mySprite = GetComponent<SpriteRenderer> ();
+		animator = GetComponent<Animator> ();
+		LevelController.current.setRebitStaringPoint (transform.position);
 	}
 	
 	// Update is called once per frame
@@ -20,6 +28,37 @@ public class HeroRabit : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
+		Vector3 from = transform.position + Vector3.up * 0.3f;
+		Vector3 to = transform.position + Vector3.down * 0.1f;
+		int layer_id = 1 << LayerMask.NameToLayer ("Ground");
+		//Перевіряємо чи проходить лінія через Collider з шаром Ground
+		RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+		if(hit) {
+			isGrounded = true;
+		} else {
+			isGrounded = false;
+		}
+		//Намалювати лінію (для розробника)
+		Debug.DrawLine (from, to, Color.red);
+
+		if(Input.GetButtonDown("Jump") && isGrounded) {
+			this.JumpActive = true;
+		}
+		if(this.JumpActive) {
+			//Якщо кнопку ще тримають
+			if(Input.GetButton("Jump")) {
+				this.JumpTime += Time.deltaTime;
+				if (this.JumpTime < this.MaxJumpTime) {
+					Vector2 vel = myBody.velocity;
+					vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+					myBody.velocity = vel;
+				}
+			} else {
+				this.JumpActive = false;
+				this.JumpTime = 0;
+			}
+		}
+
 		float value = Input.GetAxis ("Horizontal");
 
 		if (Mathf.Abs (value) > 0) {
@@ -31,6 +70,18 @@ public class HeroRabit : MonoBehaviour {
 			mySprite.flipX = true;
 		else if (value > 0)
 			mySprite.flipX = false;
+		
+		if(Mathf.Abs(value) > 0) {
+			animator.SetBool ("run", true);
+		} else {
+			animator.SetBool ("run", false);
+		}
+
+		if(this.isGrounded) {
+			animator.SetBool ("jump", false);
+		} else {
+			animator.SetBool ("jump", true);
+		}
 	}
 
 
---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class LevelController : MonoBehaviour {
+	public static LevelController current;
+	void Awake() {
+		current = this;
+	}
+	Vector3 startingPoint;
+
+	public void setRebitStaringPoint(Vector3 pos){
+		startingPoint = pos;
+	}
+
+	public void onRabitDeath(HeroRabit rabit){
+		rabit.transform.position = this.startingPoint;
+	}
+}
